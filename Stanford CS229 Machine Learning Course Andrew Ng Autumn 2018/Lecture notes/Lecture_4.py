9e72c15c45d178bd86efd69207ee8c7dd868cc00
TZ-Boomer <julienmarty0819@gmail.com>
2024-06-10 16:28:51 +0200

    Lecture 5

--- Stanford CS229 Machine Learning Course Andrew Ng Autumn 2018/Lecture notes/Lecture_4.py
+++ Stanford CS229 Machine Learning Course Andrew Ng Autumn 2018/Lecture notes/Lecture_4.py
@@ -152,3 +152,27 @@ for example_index in range(n):
 
 print("\nCross Entropy Loss of the full dataset : \n", cross_entropy_loss(logits_after_trainig, y))
 
+
+
+
+
+""" 
+CHAPTER 3
+Generalized linear models
+"""
+
+"""
+Use distributions that are members of the exponential family and write it as an exponential family (main notes p29).
+Exampl of distributions :
+Bernoulli 
+Gaussian
+Multinomial
+Poisson
+Gamma
+Exponential
+Beta 
+Dirichlet
+...
+
+Construct a model (GLM) to resolve a classification or regression problem from the exponential family
+"""
